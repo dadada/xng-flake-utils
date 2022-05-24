5a3be76e003bf422649e924180d5191ef4fd18ce
Wanja Zaeske <wanja.zaeske@dlr.de>
2022-05-24 13:39:33 +0200

    add buildSkeOps function

--- flake.nix
+++ flake.nix
@@ -3,17 +3,30 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."${system}";
+      stdenv = pkgs.gcc49Stdenv; # TODO is this really necessary?
+      customPython = (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [ lxml ]));
     in
     {
-      # build an XNG OPS from a tarball release
-      lib.buildXngOps = { pkgs, src }: pkgs.stdenv.mkDerivation {
+      # build an SKE OPS from a tarball release
+      lib.builSkeOps = { pkgs, src }: stdenv.mkDerivation {
         name = "xng-ops";
         inherit src;
         nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-        buildInputs = with pkgs; [
-          (python3.withPackages (pythonPackages: with pythonPackages; [ lxml ]))
-          libxml2
-        ];
+        buildInputs = with pkgs; [ customPython libxml2 ];
+        installPhase = ''
+          runHook preInstall
+          mkdir $out
+          cp -r . $out/
+          runHook postInstall
+        '';
+      };
+
+      # build an XNG OPS from a tarball release
+      lib.buildXngOps = { pkgs, src }: stdenv.mkDerivation {
+        name = "xng-ops";
+        inherit src;
+        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+        buildInputs = with pkgs; [ customPython libxml2 ];
         installPhase = ''
           runHook preInstall
           mkdir $out
@@ -23,7 +36,7 @@
       };
 
       # compile one XNG configuration
-      lib.buildXngConfig = { pkgs, xngOps, name, src, xngConfigurationPath }: pkgs.stdenv.mkDerivation {
+      lib.buildXngConfig = { pkgs, xngOps, name, src, xngConfigurationPath }: stdenv.mkDerivation {
         inherit name src;
         nativeBuildInputs = [ pkgs.gcc-arm-embedded xngOps ];
         XNG_ROOT_PATH = xngOps;
